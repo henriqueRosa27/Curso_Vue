c427d4802751c8fa9c9518640ec0d291c9112a23
PC-Trabalhos <henrique.rosa07@outlook.com.br>
2019-06-10 01:07:25 -0300

    VUE CLI - Projeto

---
+++ VUE_CLI/project_school/src/components/Aluno/Alunos.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <titulo texto="Aluno"/>
+    <input type="text" placeholder="Nome do Aluno" v-model="nome" v-on:keyup.enter="AddAluno()">
+    <button class="btn btn-input" @click="AddAluno()">Adicionar Aluno</button>
+    <table>
+      <thead>
+        <th>Mat.</th>
+        <th>Nome</th>
+        <th>Opções</th>
+      </thead>
+      <tbody v-if="alunos.length">
+        <tr v-for="( aluno, index) in alunos" :key="index">
+          <!-- <td>{{index + 1}}</td> -->
+          <td>{{aluno.id}}</td>
+          <td>{{aluno.nome}} {{aluno.sobrenome}}</td>
+          <td>
+            <button class="btn btn-danger" @click="Remover(aluno)">Remover</button>
+          </td>
+        </tr>
+      </tbody>
+      <!-- <tfoot v-if="!alunos.length">Nenhum Aluno encontrado</tfoot> -->
+      <tfoot v-else>Nenhum Aluno encontrado</tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+import Titulo from "../_share/Titulo";
+export default {
+  components: {
+    Titulo
+  },
+  //name: "Aluno",
+  data() {
+    return {
+      titulo: "Aluno",
+      nome: "",
+      alunos: []
+      /*alunos: [
+        
+      ]*/
+    };
+  },
+  created() {
+    this.$http
+      .get("http://localhost:3000/aluno")
+      .then(res => res.json())
+      .then(alunos => (this.alunos = alunos));
+  },
+  props: {},
+  methods: {
+    AddAluno() {
+      let _aluno = {
+        nome: this.nome,
+        sobrenome: ""
+      };
+
+      this.$http
+        .post("http://localhost:3000/aluno", _aluno)
+        .then(res => res.json())
+        .then(aluno => {
+          this.alunos.push(aluno);
+          this.nome = "";
+        });
+
+      //this.alunos.push(_aluno);
+      /*console.log("---------------");
+      this.alunos.forEach(aluno => {
+        console.log(aluno);
+      });*/
+      //this.nome = "";
+    },
+    Remover(aluno) {
+      this.$http.delete(`http://localhost:3000/aluno/${aluno.id}`).then(() => {
+        let indice = this.alunos.indexOf(aluno);
+        this.alunos.splice(indice, 1);
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input {
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  color: #677f7f;
+  display: inline;
+}
+.btn-input {
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  display: inline;
+  background-color: rgb(116, 115, 115);
+}
+
+.btn-input:hover {
+  padding: 20px;
+  margin: 0px;
+  border: 0;
+}
+</style>
