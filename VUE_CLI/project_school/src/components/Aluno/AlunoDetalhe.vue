be7cf4127579e8fd1c4ce91f1778d83bc6d2cebb
John Doe <henrique.rosa@meta.com.br>
2019-06-18 13:20:15 -0300

    :)

--- VUE_CLI/project_school/src/components/Aluno/AlunoDetalhe.vue
+++ VUE_CLI/project_school/src/components/Aluno/AlunoDetalhe.vue
@@ -1,6 +1,50 @@
 <template>
   <div>
     <titulo :texto="`Aluno: ${aluno.nome}`"/>
+    <table>
+      <tbody>
+        <tr>
+          <td class="colPequeno">Matrícula:</td>
+          <td>
+            <label>{{aluno.id}}</label>
+          </td>
+        </tr>
+        <tr>
+          <td>Nome:</td>
+          <td>
+            <label>{{aluno.nome}}</label>
+            <input v-model="aluno.nome" type="text">
+          </td>
+        </tr>
+        <tr>
+          <td>Sobrenome:</td>
+          <td>
+            <label>{{aluno.sobrenome}}</label>
+            <input v-model="aluno.sobrenome" type="text">
+          </td>
+        </tr>
+        <tr>
+          <td>Data Nascimento:</td>
+          <td>
+            <label>{{aluno.dataNasc}}</label>
+            <input v-model="aluno.dataNasc" type="text">
+          </td>
+        </tr>
+        <tr>
+          <td>Professor:</td>
+          <td>
+            <label>{{aluno.professor.nome}}</label>
+            <select v-model="aluno.professor">
+              <option
+                v-for="(professor, index) in professores"
+                :key="index"
+                v-bind:value="professor"
+              >{{professor.nome}}</option>
+            </select>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -12,7 +56,8 @@ export default {
   },
   data() {
     return {
-      aluno: {},
+      professores: [],
+      aluno: [],
       id: this.$route.params.id
     };
   },
@@ -21,6 +66,11 @@ export default {
       .get("http://localhost:3000/aluno/" + this.id)
       .then(res => res.json())
       .then(aluno => (this.aluno = aluno));
+
+    this.$http
+      .get("http://localhost:3000/professores")
+      .then(res => res.json())
+      .then(professor => (this.professores = professor));
   },
   methods: {}
 };
